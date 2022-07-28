88c10bf5edd822d02ba2c8ad5ee924be8bae175b
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-28 13:31:13 +0200

    Initial Commit

---
+++ Continuations/ContinuationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuationsApp.swift
+//  Continuations
+//
+//  Created by Skorobogatow, Christian on 28/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ContinuationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
